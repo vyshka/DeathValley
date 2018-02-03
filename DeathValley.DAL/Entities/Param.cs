45ed44e10df8d734fe042e39b6e6c955dc1a09e4
vyshka <egorvyshka@gmail.com>
2018-02-03 15:31:42 +0300

    entities models added

---
+++ DeathValley.DAL/Entities/Param.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeathValley.DAL.Entities
+{
+    class Param
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ParamId { get; set; }
+
+        public double CoefficientA { get; set; }
+        public double CoefficientB { get; set; }
+        public double CoefficientC { get; set; }
+        public double Step { get; set; }
+        public double RangeFrom { get; set; }
+        public double  RangeTo { get; set; }
+    }
+}
